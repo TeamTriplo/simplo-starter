8f9353a8b67ddf84ffd86edc6bcd531da4f82b0c
Rajib Paul <paul.rajib03@gmail.com>
2021-02-23 19:17:03 +0600

    SPPX session logout through API

--- src/modules/triplo/crowdfunding_campaign/templates/block-sppx-session-info.tpl.php
+++ src/modules/triplo/crowdfunding_campaign/templates/block-sppx-session-info.tpl.php
@@ -1,1 +1,7 @@
-<div>You have an open session with SPPX. When you are done, <a href="/logout">[Logout]</a> to end your session.</div>
+<?php if($access_token ) : ?>
+<div class="sppx-session">
+You have an open session with SPPX. When you are done, 
+<a class="sppx-session-logout" href="/sppx-logout">[Logout]</a> 
+to end your session.
+</div>
+<?php endif;?>
