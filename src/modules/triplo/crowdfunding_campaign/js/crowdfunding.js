1f698bccde7f2867ceb34df1e0cdd94c6cbf501a
Tim Erickson <tim@triplo.co>
2021-03-01 01:36:43 -0600

    Adding code for logout

--- src/modules/triplo/crowdfunding_campaign/js/crowdfunding.js
+++ src/modules/triplo/crowdfunding_campaign/js/crowdfunding.js
@@ -4,5 +4,30 @@ jQuery(document).ready(function($) {
       $(".hero_image_wrapper").insertAfter("header.l-header");
       $("h1.page-title").appendTo(".hero_image_wrapper .hero_page_title .container");
     }
+    
+    $('.sppx-session-logout').click(function(){
+      var confirmed = confirm("Do you really want to log out?");
+      if(confirmed){
+        // AJAX POST request to SPPX 
+        $.ajax({
+          url: "/sppx-logout",
+          type: "post",
+          success: function (response) {
+            let result = JSON.parse(response);
+            response_code =  result['status']['code'];
+            if(response_code != '200'){
+              alert(result['status']['memo']);
+            }else{
+              location.href = location.href
+            }
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+          }
+        });
+      }
+      return false;
+    });
+
   });
 });
---
+++ src/modules/triplo/crowdfunding_campaign/templates/block-sppx-session-info.tpl.php
@@ -0,0 +1,7 @@
+<?php if($access_token ) : ?>
+<div class="sppx-session">
+You have an open session with SPPX. When you are done, 
+<a class="sppx-session-logout" href="/sppx-logout">[Logout]</a> 
+to end your session.
+</div>
+<?php endif;?>
